96c6cb351aa1c841ba7be3acba9e17b28675968f
Rafał Mażuchowski <mazuchowski.r@gmail.com>
2019-10-14 18:08:48 +0200

    Nowy zbior: 5, Utworzenie zadania 5.1 i 5.2

---
+++ src/r5_petleITablice/z1_choinka/Choinka.java
@@ -0,0 +1,22 @@
+package r5_petleITablice.z1_choinka;
+
+import java.util.Scanner;
+
+public class Choinka {
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		System.out.println("Podaj wielkoœæ choinki");
+		sc = new Scanner(System.in);
+		int wielkosc = sc.nextInt();
+		for (int i = 0; i < wielkosc; i++) {
+			for (int j = wielkosc; j > i; j--) {
+				System.out.print("  ");
+			}
+			for (int k = 1; k <= 2 * i + 1; k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ src/r5_petleITablice/z2_operacjeNaTablicachLiczb/OperacjeNaTablicachLiczb.java
@@ -0,0 +1,5 @@
+package r5_petleITablice.z2_operacjeNaTablicachLiczb;
+
+public class OperacjeNaTablicachLiczb {
+	
+}
