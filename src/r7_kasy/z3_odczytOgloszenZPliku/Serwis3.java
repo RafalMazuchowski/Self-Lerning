7ac4590ba831f7fd2eba51adb9a477b2800b14b2
Rafał Mażuchowski <mazuchowski.r@gmail.com>
2019-11-10 16:38:44 +0100

    Modyfikacja: Zadanie 7.3

--- src/r7_kasy/z3_odczytOgloszenZPliku/Serwis3.java
+++ src/r7_kasy/z3_odczytOgloszenZPliku/Serwis3.java
@@ -3,54 +3,52 @@ package r7_kasy.z3_odczytOgloszenZPliku;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import r7_kasy.z1_ogloszenia.OgloszenieMieszkaniowe;
 
 public class Serwis3 {
+	public static final String SEPARATOR = "\n";
 
 	public static void main(String[] args) {
 		ArrayList<OgloszenieMieszkaniowe> listaMieszkan = new ArrayList<OgloszenieMieszkaniowe>();
-		
+
 		dodawanieElemListy(listaMieszkan);
-		
+
 		zapisMieszkan(listaMieszkan);
+		
 		wczytanieMieszkan();
-/*
-		System.out.println("Ogloszenia z przedzialu cenowego 50'000 do 100'000:");
-		for (OgloszenieMieszkaniowe str : listaMieszkan) {
-			if (str.getCena() > 50000 && str.getCena() <= 100000) {
-				System.out.println(str);
-			}
-		}
-		
-		System.out.println();
-		
-		System.out.println("Ogloszenia z San diego z metrarzem +100:");
-		for (OgloszenieMieszkaniowe str : listaMieszkan) {
-			if (str.getMetraz() >= 100 && str.getMiejscowosc() == "San Diego") {
-				System.out.println(str.getTytul() + ", Cena: " + str.getCena());
-			}
-		}*/
-		
+
 	}
 
 	private static void wczytanieMieszkan() {
 		File plik = new File("listaMieszkan.csv");
-		
+
 		System.out.println("Czytam dane...");
-		
+
 		List<OgloszenieMieszkaniowe> mieszkania = ObslugaCSV.wczytajMieszkania(plik);
+
 		System.out.println("Wczytalem " + mieszkania.size() + " ofet mieszkan:");
-		for(OgloszenieMieszkaniowe mieszkanie : mieszkania) {
-			System.out.println(mieszkanie);
-		}
+
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Gotowe");
+		System.out.println("Jaka ma byæ MINimalna cena mieszkania?");
+		int cenaMin = sc.nextInt();
+		System.out.println("Jaka ma byæ MAXymalna cena mieszkania?");
+		int cenaMax = sc.nextInt();
+
+		for (OgloszenieMieszkaniowe mieszkanie : mieszkania) {
+			if (mieszkanie.getCena() >= cenaMin && (mieszkanie.getCena() <= cenaMax)) {
+				System.out.println(mieszkanie);
+			}
+		}
+
 	}
 
 	private static void dodawanieElemListy(ArrayList<OgloszenieMieszkaniowe> listaMieszkan) {
 		listaMieszkan.add(
-				new OgloszenieMieszkaniowe("Willa", "Sprzedam dom willowy", "hugon@hail.de", 145000,"Breslow", 260, 7));
+				new OgloszenieMieszkaniowe("Willa", "Sprzedam dom willowy", "hugon@hail.de", 145000, "Breslow", 260, 7));
 		listaMieszkan.add(
 				new OgloszenieMieszkaniowe("Domek", "Sprzedam maly domek", "frodo@bro.pl", 55000, "Tomaszow", 90, 3));
 		listaMieszkan.add(
@@ -58,23 +56,22 @@ public class Serwis3 {
 		listaMieszkan.add(
 				new OgloszenieMieszkaniowe("Szalas", "Dobre miejsce na impreze", "874-465-862", 7400,"San Diego", 10, 1));
 		listaMieszkan.add(
-				new OgloszenieMieszkaniowe("Mieszkanie", "Sprzedam mieszkanie", "trusku@gmail.com", 37000,	"Wroclaw", 330, 4));
+				new OgloszenieMieszkaniowe("Mieszkanie", "Sprzedam mieszkanie", "trusku@gmail.com", 37000, "Wroclaw", 330, 4));		
 	}
 
 	private static void zapisMieszkan(ArrayList<OgloszenieMieszkaniowe> listaMieszkan) {
 		System.out.println("Pracownicy do zapisania:");
-		for(OgloszenieMieszkaniowe mieszkanie : listaMieszkan) {
+		for (OgloszenieMieszkaniowe mieszkanie : listaMieszkan) {
 			System.out.println(mieszkanie);
 		}
-		
+
 		System.out.println("Zapisuje do pliku SCV...");
 
-		
 		File plik = new File("listaMieszkan.csv");
-		
+
 		ObslugaCSV.zapiszMieszkania(listaMieszkan, plik);
-		
-		System.out.println("Gotowe");
+
+		System.out.println("Gotowe\n");
 	}
 
 }
