cdce32cfaee3c75d6f46f21b36043f7a5135897f
Rafał Mażuchowski <mazuchowski.r@gmail.com>
2019-11-10 14:19:47 +0100

    Modyfikacja zadania 7.3

--- src/r7_kasy/z3_odczytOgloszenZPliku/Serwis3.java
+++ src/r7_kasy/z3_odczytOgloszenZPliku/Serwis3.java
@@ -2,6 +2,7 @@ package r7_kasy.z3_odczytOgloszenZPliku;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 import r7_kasy.z1_ogloszenia.OgloszenieMieszkaniowe;
 
@@ -10,30 +11,10 @@ public class Serwis3 {
 	public static void main(String[] args) {
 		ArrayList<OgloszenieMieszkaniowe> listaMieszkan = new ArrayList<OgloszenieMieszkaniowe>();
 		
-		listaMieszkan.add(new OgloszenieMieszkaniowe("Willa", "Sprzedam dom willowy", "hugon@hail.de", 145000,
-				"Breslow", 260, 7));
-		listaMieszkan.add(
-				new OgloszenieMieszkaniowe("Domek", "Sprzedam maly domek", "frodo@bro.pl", 55000, "Tomaszow", 90, 3));
-		listaMieszkan.add(new OgloszenieMieszkaniowe("Penthause", "Luksusowy Penthause", "874-465-862", 7400000,
-				"San Diego", 730, 12));
-		listaMieszkan.add(new OgloszenieMieszkaniowe("Szalas", "Dobre miejsce na impreze", "874-465-862", 7400,
-				"San Diego", 10, 1));
-		listaMieszkan.add(new OgloszenieMieszkaniowe("Mieszkanie", "Sprzedam mieszkanie", "trusku@gmail.com", 37000,
-				"Wroclaw", 330, 4));
+		dodawanieElemListy(listaMieszkan);
 		
-		System.out.println("Pracownicy do zapisania:");
-		for(OgloszenieMieszkaniowe mieszkanie : listaMieszkan) {
-			System.out.println(mieszkanie);
-		}
-		
-		System.out.println("Zapisuje do pliku SCV...");
-
-		
-		File plik = new File("listaMieszkan.csv");
-		
-		ObslugaCSV.zapiszMieszkania(listaMieszkan, plik);
-		
-		System.out.println("Gotowe");
+		zapisMieszkan(listaMieszkan);
+		wczytanieMieszkan();
 /*
 		System.out.println("Ogloszenia z przedzialu cenowego 50'000 do 100'000:");
 		for (OgloszenieMieszkaniowe str : listaMieszkan) {
@@ -50,6 +31,50 @@ public class Serwis3 {
 				System.out.println(str.getTytul() + ", Cena: " + str.getCena());
 			}
 		}*/
+		
+	}
+
+	private static void wczytanieMieszkan() {
+		File plik = new File("listaMieszkan.csv");
+		
+		System.out.println("Czytam dane...");
+		
+		List<OgloszenieMieszkaniowe> mieszkania = ObslugaCSV.wczytajMieszkania(plik);
+		System.out.println("Wczytalem " + mieszkania.size() + " ofet mieszkan:");
+		for(OgloszenieMieszkaniowe mieszkanie : mieszkania) {
+			System.out.println(mieszkanie);
+		}
+		
+		System.out.println("Gotowe");
+	}
+
+	private static void dodawanieElemListy(ArrayList<OgloszenieMieszkaniowe> listaMieszkan) {
+		listaMieszkan.add(
+				new OgloszenieMieszkaniowe("Willa", "Sprzedam dom willowy", "hugon@hail.de", 145000,"Breslow", 260, 7));
+		listaMieszkan.add(
+				new OgloszenieMieszkaniowe("Domek", "Sprzedam maly domek", "frodo@bro.pl", 55000, "Tomaszow", 90, 3));
+		listaMieszkan.add(
+				new OgloszenieMieszkaniowe("Penthause", "Luksusowy Penthause", "874-465-862", 7400000, "San Diego", 730, 12));
+		listaMieszkan.add(
+				new OgloszenieMieszkaniowe("Szalas", "Dobre miejsce na impreze", "874-465-862", 7400,"San Diego", 10, 1));
+		listaMieszkan.add(
+				new OgloszenieMieszkaniowe("Mieszkanie", "Sprzedam mieszkanie", "trusku@gmail.com", 37000,	"Wroclaw", 330, 4));
+	}
+
+	private static void zapisMieszkan(ArrayList<OgloszenieMieszkaniowe> listaMieszkan) {
+		System.out.println("Pracownicy do zapisania:");
+		for(OgloszenieMieszkaniowe mieszkanie : listaMieszkan) {
+			System.out.println(mieszkanie);
+		}
+		
+		System.out.println("Zapisuje do pliku SCV...");
+
+		
+		File plik = new File("listaMieszkan.csv");
+		
+		ObslugaCSV.zapiszMieszkania(listaMieszkan, plik);
+		
+		System.out.println("Gotowe");
 	}
 
 }
